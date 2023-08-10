55b98851792430013afa26b939a254f5bf05ed07
flauer <flauer@student.42heilbronn.de>
2023-08-10 12:32:53 +0200

    adsf

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 13:04:24 by flauer            #+#    #+#             */
-/*   Updated: 2023/08/10 10:26:50 by flauer           ###   ########.fr       */
+/*   Updated: 2023/08/10 12:17:04 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(int argc, char **argv, char **env)
 	t_shell	shell;
 
 	init(&shell, argc, argv, env);
+	signal_handler(); // handle signals
 	main_loop(&shell);
 	cleanup(&shell);
 	return (EXIT_SUCCESS);
