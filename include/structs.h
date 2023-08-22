6b181fe3d69afa6d221db47a1362f34b872f7916
pcazac <pcazac@student.42heilbronn.de>
2023-08-22 11:12:50 +0200

    Structures added

--- include/structs.h
+++ include/structs.h
@@ -6,34 +6,57 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 13:07:41 by flauer            #+#    #+#             */
-/*   Updated: 2023/08/21 17:26:07 by flauer           ###   ########.fr       */
+/*   Updated: 2023/08/22 11:20:06 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
-typedef struct s_shell
+#include "../libft/include/libft.h"
+
+typedef struct s_var
 {
-	char	**env;
-	char	**tokens;
-}	t_shell;
+	char	*name;
+	char	*val;
+}	t_var;
 
 typedef enum
 {
-	NODE_CMD,
+	NODE_EXEC,
 	NODE_PIPE,
 	NODE_REDIRECT
 }	t_nodetype;
 
-typedef struct s_astnode
+typedef struct s_cmd
 {
-	t_nodetype			type;
-	char				*cmd;
-	char				**argv;
-	char				**env;
-	struct s_astnode	*left;
-	struct s_astnode	*right;
-}	t_astnode;
+	t_nodetype	type;
+}	t_cmd;
+
+typedef struct s_exec
+{
+	t_nodetype	type;
+	char		*cmd;
+	char		**argv;
+	char		**eargv;
+	char		**env;
+}	t_exec;
+
+typedef struct s_pipe
+{
+	t_nodetype	type;
+	t_cmd		*left;
+	t_cmd		*right;
+}	t_pipe;
+
+typedef struct s_redir
+{
+	t_nodetype	type;
+	t_cmd		*cmd;
+	char		*file;
+	char		*efile;
+	int			mode
+	int			fd;
+}	t_redir;
 
 #endif
