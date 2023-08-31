6276500fc80bb3b3d5a2d09e4b775bc7faf83fb5
flauer <flauer@student.42heilbronn.de>
2023-08-31 11:14:17 +0200

    testing the executor

--- src/core/minishell.c
+++ src/core/minishell.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 13:04:24 by flauer            #+#    #+#             */
-/*   Updated: 2023/08/29 17:51:21 by flauer           ###   ########.fr       */
+/*   Updated: 2023/08/31 11:04:48 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +50,55 @@ int	main(int argc, char **argv, char **env)
 	t_exec	exec;
 	exec.type = NODE_EXEC;
 	exec.pid = -1;
-	exec.cmd = "ls";
-	exec.argv = (char *[]){"ls", "-l", NULL};
+	exec.cmd = "grep";
+	exec.argv = (char *[]){"grep", "Petru", NULL};
 	exec.eargv = (char *[]){NULL};
 	exec.sh = &sh;
-	root = (t_cmd *)&exec;
+	t_exec	exe2;
+	exe2.type = NODE_EXEC;
+	exe2.pid = -1;
+	exe2.cmd = "wc";
+	exe2.argv = (char *[]){"wc", "-l", NULL};
+	exe2.eargv = (char *[]){NULL};
+	exe2.sh = &sh;
+	t_redir	redir;
+	redir.type = NODE_REDIRECT;
+	redir.pid = -1;
+	redir.cmd = (t_cmd *)&exe2;
+	redir.file = "out.txt";
+	redir.efile = NULL;
+	redir.mode = O_WRONLY | O_CREAT | O_TRUNC;
+	redir.fd = 0;
+	t_pipe	pipe;
+	pipe.type = NODE_PIPE;
+	pipe.pid = -1;
+	pipe.left = (t_cmd *)&exec;
+	pipe.right = (t_cmd *)&redir;
+	root = (t_cmd *)&pipe;
+	t_exec	exe3;
+	exe3.type = NODE_EXEC;
+	exe3.pid = -1;
+	exe3.cmd = "cat";
+	exe3.argv = (char *[]){"cat", NULL};
+	exe3.eargv = (char *[]){NULL};
+	exe3.sh = &sh;
+	t_redir	red2;
+	red2.type = NODE_REDIRECT;
+	red2.pid = -1;
+	red2.cmd = (t_cmd *)&exe3;
+	red2.file = "LIM";
+	red2.efile = NULL;
+	red2.mode = O_HEREDOC;
+	red2.fd = 0;
+	t_pipe	pip2;
+	pip2.type = NODE_PIPE;
+	pip2.pid = -1;
+	pip2.left = (t_cmd *)&red2;
+	pip2.right = (t_cmd *)&pipe;
+	root = (t_cmd *)&pip2;
 	execute(root);
+	// printf("exit code: %i\n", sh.ret);
 	return (0);
 }
+
+// <asf.txt cat | grep Petru | wc -l >out.txt
