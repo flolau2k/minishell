bb6c88b007fc385fc0983526fc50df2c7db92325
flauer <flauer@student.42heilbronn.de>
2023-09-07 14:09:35 +0200

    fix double prompt on CTRL-C when breaking a command

--- src/core/signals.c
+++ src/core/signals.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 17:01:15 by flauer            #+#    #+#             */
-/*   Updated: 2023/09/07 11:18:47 by flauer           ###   ########.fr       */
+/*   Updated: 2023/09/07 14:08:24 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	signal_handler(int signo)
 {
 	g_sig = signo;
 	printf("\n");
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	if (rl_line_buffer[0] == '\0') {
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
