5dd493d108a3dae2654329417bfd85a835b90b46
flauer <flauer@student.42heilbronn.de>
2023-09-15 14:06:40 +0200

    remove unused

--- src/core/signals.c
+++ src/core/signals.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 17:01:15 by flauer            #+#    #+#             */
-/*   Updated: 2023/09/15 13:51:07 by flauer           ###   ########.fr       */
+/*   Updated: 2023/09/15 14:06:34 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	signal_handler(int signo)
 {
 	g_sig = signo;
-	// printf("\n");
 	rl_reset_line_state();
 	rl_cleanup_after_signal();
 	rl_replace_line("", 0);
