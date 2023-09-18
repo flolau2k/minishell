f8233ad2bcd51f3facce04ecdee3f11166596931
flauer <flauer@student.42heilbronn.de>
2023-09-18 10:25:03 +0200

    fix only use allowed funcs

--- src/core/signals.c
+++ src/core/signals.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 17:01:15 by flauer            #+#    #+#             */
-/*   Updated: 2023/09/15 14:06:34 by flauer           ###   ########.fr       */
+/*   Updated: 2023/09/18 10:24:51 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 void	signal_handler(int signo)
 {
 	g_sig = signo;
-	rl_reset_line_state();
-	rl_cleanup_after_signal();
 	rl_replace_line("", 0);
-	rl_crlf();
+	rl_on_new_line();
+	printf("\n");
 	rl_redisplay();
 }
