28fea2b9927d9eef39d851674d19c9a4ca42f1b0
flauer <flauer@student.42heilbronn.de>
2023-09-28 13:38:36 +0200

    fix export multiple args

--- src/builtins/f_export.c
+++ src/builtins/f_export.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:10:33 by flauer            #+#    #+#             */
-/*   Updated: 2023/09/28 09:33:41 by flauer           ###   ########.fr       */
+/*   Updated: 2023/09/28 13:36:11 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,31 @@ int	f_export(t_exec *cmd)
 {
 	char	*key;
 	char	*new;
+	int		i;
 
-	new = cmd->argv[1];
-	if (!new)
-		return (f_env(cmd));
-	if (ft_strchr(new, '='))
+	i = 1;
+	while (cmd->argv[i])
 	{
-		key = ft_substr(new, 0, ft_strchr(new, '=') - new);
-		if (!is_valid_identfier(key))
+		new = cmd->argv[i];
+		if (ft_strchr(new, '='))
 		{
-			free_exec(cmd);
-			ft_error3(key, "not a valid identifier", BUILTIN_MISUSE, key);
+			key = ft_substr(new, 0, ft_strchr(new, '=') - new);
+			if (!is_valid_identfier(key))
+			{
+				ft_error2(key, "not a valid identifier", key);
+				i++;
+				continue ;
+			}
+			if (get_env(cmd->sh->env, key))
+				replace_in_env(cmd->sh->env, new);
+			else
+				cmd->sh->env = set_env(cmd->sh->env, new);
+			free(key);
 		}
-		if (get_env(cmd->sh->env, key))
-		{
-			if (!replace_in_env(cmd->sh->env, new))
-				ft_error3(NULL, "failure in replacing env!", GENERAL_ERROR, key);
-		}
-		else
-			cmd->sh->env = set_env(cmd->sh->env, new);
-		free(key);
+		i++;
 	}
+	if (i == 1)
+		return (f_env(cmd));
 	free_exec(cmd);
 	return (EXIT_SUCCESS);
 }
