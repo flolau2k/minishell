b3897d338cc078e4529497ff9b77fd1a004eb042
flolau2k <florian.lauer@live.de>
2023-09-29 15:21:29 +0200

    update PWD

--- src/builtins/f_cd.c
+++ src/builtins/f_cd.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:10:31 by flauer            #+#    #+#             */
-/*   Updated: 2023/09/29 14:23:56 by flauer           ###   ########.fr       */
+/*   Updated: 2023/09/29 15:20:04 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@ static void	set_oldpwd(t_exec *cmd, char *oldpwd)
 {
 	char	*new;
 	char	*key;
+	char	*cwd[2];
 
 	new = ft_strjoin("OLDPWD=", oldpwd);
 	key = ft_strdup("OLDPWD");
 	cmd->sh->env = put_in_env(cmd->sh->env, new, key);
+	cwd[0] = getcwd(NULL, 0);
+	cwd[1] = ft_strjoin("PWD=", cwd[0]);
 	free(new);
 	free(oldpwd);
+	replace_in_env(cmd->sh->env, cwd[1]);
+	free(cwd[0]);
+	free(cwd[1]);
 }
 
 static int	cd_error(t_exec *cmd, char *dir)
