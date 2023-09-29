26d67d8fc2aa03371351af0179a5a2f84f310c53
flolau2k <florian.lauer@live.de>
2023-09-29 15:25:14 +0200

    fix unset multiple args

--- src/builtins/f_unset.c
+++ src/builtins/f_unset.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:10:35 by flauer            #+#    #+#             */
-/*   Updated: 2023/09/29 12:46:54 by flauer           ###   ########.fr       */
+/*   Updated: 2023/09/29 15:24:38 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	f_unset(t_exec *cmd)
 {
+	int	i;
+	
+	i = 0;
 	cmd->sh->ret = 0;
-	if (cmd->argv[1])
+	while (cmd->argv[i])
 	{
-		if (!is_valid_identfier(cmd->argv[1], false))
-			ft_error4(cmd->sh, cmd->argv[1], "not a valid identifier", NULL);
+		if (!is_valid_identfier(cmd->argv[i], false))
+			ft_error4(cmd->sh, cmd->argv[i], "not a valid identifier", NULL);
 		else
-			cmd->sh->env = unset_env(cmd->sh->env, cmd->argv[1]);
+			cmd->sh->env = unset_env(cmd->sh->env, cmd->argv[i]);
+		i++;
 	}
 	free_exec(cmd);
 	return (cmd->sh->ret);
